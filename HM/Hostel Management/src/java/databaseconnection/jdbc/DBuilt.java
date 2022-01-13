9f3f314c7ae05c0ce7b1db67efa134ec348e40c5
NikhilChigurumamidi <chigurumamidinikhil8@gmail.com>
2022-01-13 17:03:19 +0530

    new

---
+++ HM/Hostel Management/src/java/databaseconnection/jdbc/DBuilt.java
@@ -0,0 +1,15 @@
+package jdbc;
+
+
+import java.net.URISyntaxException;
+
+public class DButil {
+
+Connection connection=null;
+
+ResourceBundle db_parm;
+
+public static Connection getConnection() throws URISyntaxException, SQLException
+{ String dbUrl= System.getenv("JDBC_DATABASE_URL"); 
+return DriverManager.getConnection(dburl); 
+}}
